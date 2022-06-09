75c00e64001208ea613f37f97b21da4645291293
marina-kuz <kusmari@mail.ru>
2022-06-09 17:07:17 +0700

    first comm

---
+++ App_/App_/Context.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace App_
+{
+    class Context :DbContext
+    {
+        public DbSet<History> Histories { get; set; }
+        public DbSet<Person> People { get; set; }
+
+        public Context()
+        {
+            Database.EnsureCreated();
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("Filename=DataBase.db");
+        }
+    }
+}
---
+++ App_/App_/MainPage.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+namespace App_
+{
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private void NavigationView_Loaded(object sender, RoutedEventArgs e)
+        {
+            ContentFrame.Navigate(typeof(AmountPage));
+        }
+
+        private void NavigationView_SelectionChanged(NavigationView sender, NavigationViewSelectionChangedEventArgs args)
+        {
+            if(args.IsSettingsSelected)
+            {
+
+            }
+            else
+            {
+                NavigationViewItem item = args.SelectedItem as NavigationViewItem;
+                switch(item.Tag.ToString())
+                {
+                    case "Amount":
+                        ContentFrame.Navigate(typeof(AmountPage));
+                        break;
+                    case "Operation":
+                        ContentFrame.Navigate(typeof(NewOperationPage));
+                        break;
+                    case "History":
+                        ContentFrame.Navigate(typeof(HistoryPage));
+                        break;
+                }
+            }
+
+        }
+    }
+}
